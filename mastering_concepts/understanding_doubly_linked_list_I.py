889e5c6f045ae61038a8f25e3f413308a704e57a
Oyebolu Daniel <danylekan@gmail.com>
2020-04-05 09:46:53 +0100

    My Concepts Mastering Process

---
+++ mastering_concepts/understanding_doubly_linked_list_I.py
@@ -0,0 +1,174 @@
+class Node:
+
+    def __init__(self, data, prev=None, next=None):
+        self.data = data
+        self.prev = prev
+        self.next = next
+
+    def get_data(self):
+        return self.data
+
+    def get_prev(self):
+        return self.prev
+
+    def get_next(self):
+        return self.next
+
+    def set_data(self, new_data):
+        self.data = new_data
+
+    def set_next(self, new_next):
+        self.next = new_next
+
+    def set_prev(self, new_prev):
+        self.prev = new_prev
+
+
+class LinkedList:
+
+    def __init__(self, main_holder=None):
+        self.main_holder = main_holder
+
+    def prepend(self, data):  # This adds a new node to the beginning of the linked list
+        new_node = Node(data)
+        new_node.set_next(self.main_holder)
+
+        if self.main_holder is not None:
+            self.main_holder.prev = new_node
+
+        self.main_holder = new_node
+
+    def append(self, data):  # This adds a new node to the end of the linked list
+        current = self.main_holder
+
+        new_node = Node(data)
+        new_node.set_next(None)
+
+        while current.get_next():
+            current = current.next
+
+        if current is None:
+            new_node.set_prev(None)
+
+        else:
+            current.set_next(new_node)
+            new_node.set_prev(current)
+
+        # while current:
+        #    if current is None:
+        #        self.prepend(data)
+        #        break
+
+        #    if current.get_next() is None:
+        #        new_node = Node(data)
+        #        new_node.set_next(None)
+        #        new_node.set_prev(current)
+        #        current.set_next(new_node)
+        #        break
+
+        #    current = current.get_next()
+
+    # This is to get the number of nodes in the linked list
+    def size(self):
+        current = self.main_holder
+
+        count = 0
+
+        while current:
+            count += 1
+            current = current.get_next()
+
+        return f"The size of the LinkedList is {count}"
+
+    # This is to reverse the linked list
+    def reverse(self):
+        current = self.main_holder
+
+        hold = []
+        size = int("".join(item for item in self.size() if item in "1234567890"))
+
+        while current:
+            hold.insert(0, current.get_data())
+            current = current.get_next()
+
+            if current.get_next() is None:
+                hold.insert(0, current.get_data())
+                break
+
+        #print(hold)
+
+        count = 0
+
+        while current:
+            count += 1
+            current.set_data(hold[size - count])
+            current = current.get_prev()
+
+
+
+    def delete_node(self, data):  # used the brute-force approach i.e Checking through the linked list.
+        current = self.main_holder
+
+        prev_holder = None
+
+        while current:
+
+            if data == current.get_data():
+                if current == self.main_holder:
+                    self.main_holder = self.main_holder.get_next()
+                    self.main_holder.set_prev(None)
+                    current = current.get_next()
+
+                elif current != self.main_holder:
+                    (current.get_prev()).set_next(current.get_next())
+                    (current.get_next()).set_prev(current.get_prev())
+                    current = current.get_next()
+
+            elif data != current.get_data():
+                current = current.get_next()
+
+    def print(self):
+        current = self.main_holder
+
+        data = []
+
+        while current:
+            data.append(current.get_data())
+            current = current.get_next()
+
+        current = self.main_holder
+
+        print("\n".join(f"{num}. {item}" for num, item in enumerate(data, start=1)))
+
+    # This is to search through the linked list for specified data
+    def search(self, data):
+        current = self.main_holder
+
+        found = 0
+        nodes = []
+
+        while current:
+            if current.get_data() == data:
+                found += 1
+                nodes.append(str(current))
+
+            current = current.get_next()
+
+        return f"Found {found} in the following nodes:" \
+               + "\n" + f"\n".join([node for node in nodes])
+
+
+li = LinkedList()
+
+li.prepend("three")
+li.prepend("two")
+li.prepend("one")
+li.prepend("three")
+li.append("four")
+li.append("five")
+
+#print(li.main_holder.next.next.next.prev.data)
+#print(li.size())
+li.reverse()
+#li.print()
+print(li.search("three"))
--- mastering_concepts/understanding_linked_lists.py
+++ mastering_concepts/understanding_linked_lists.py
@@ -1,14 +1,14 @@
 class ContainerFactory:
 
-    def __init__(self, bid, next_container=None):
-        self.bid = bid
+    def __init__(self, data, next_container=None):
+        self.data = data
         self.next_container = next_container
 
-    def get_bid(self):
-        return self.bid
+    def get_data(self):
+        return self.data
 
     def set_data(self, new_value):
-        self.bid = new_value
+        self.data = new_value
 
     def get_next_container(self):
         return self.next_container
@@ -16,29 +16,29 @@ class ContainerFactory:
     def set_next_container(self, node_or_none):
         self.next_container = node_or_none
 
+
 class ContainerOfContainers:
 
     def __init__(self, main_holder=None):
         self.main_holder = main_holder  # the self here serves as an extra layer hold\holder for the main_holder
-                                        # Also, self.main_holder here serves as:
-                                        # 1. the initial None value to be setas the second value of the first container
-                                        # 2. the holder of the first and subsequent containers
-                                        # 3. the holder of the item hat will serve as the next ingredient
-                                        # It is the seed itself and it is the life itself
-                                        # It basically give all nodes the ingredient for their lifes, next_container,
-                                        # their yoke
+        # Also, self.main_holder here serves as:
+        # 1. the initial None value to be set as the second value of the first container
+        # 2. the holder of the first and subsequent containers
+        # 3. the holder of the item hat will serve as the next ingredient
+        # It is the seed itself and it is the life itself
+        # It basically give all nodes the ingredient for their lifes, next_container,
+        # their yoke
 
-    def contain_a_bid_and_a_box_if_any(self, bid):  # only bid is required here as we've got the extra ingredient for
-                                                    # each container covered by self.main_holder
-        new_container = ContainerFactory(bid)
+    def prepend(self, data):  # only data is required here as we've got the extra ingredient for
+        # each container covered by self.main_holder
+        new_container = ContainerFactory(data)
         new_container.set_next_container(self.main_holder)
         self.main_holder = new_container
 
-
-    def get_num_of_containers_or_nodes(self):
-        container = self.main_holder        # it's not the literal self.main_holder or li.main_holder that's being
-                                            # referred to here but the value of the self.main_holder or li.main_holder
-                                            # i.e the mainly held itself\itthemselves
+    def size(self):
+        container = self.main_holder  # it's not the literal self.main_holder or li.main_holder that's being
+        # referred to here but the value of the self.main_holder or li.main_holder
+        # i.e the mainly held itself\itthemselves
 
         count = 0
         while container:
@@ -47,31 +47,31 @@ class ContainerOfContainers:
 
         return count
 
-    def search_containers_for_value(self, data):            #   ADVANCED SEARCHER GIVEN TO ME
+    def search(self, data):  # ADVANCED SEARCHER GIVEN TO ME
         container = self.main_holder
 
-        count = self.get_num_of_containers_or_nodes()
+        count = self.size()
         found = 0
         found_in = []
 
-        while container:                                    ### Spectacular lesson : you can have an else statement with
-            if container.get_bid() == data:                 ###
+        while container:  ### Spectacular lesson : you can have an else statement with
+            if container.get_data() == data:  ###
                 print(f'Found data "{data}" in entry {count}')
-                found += 1                                  ###
+                found += 1  ###
                 found_in.append(str(container))
                 container = container.get_next_container()
-            else:                                           ###
-                container = container.next_container        ###
+            else:  ###
+                container = container.next_container  ###
             count -= 1
 
-        return "Found " + str(found) + " in : \n" + " \n".join(found_in) if found > 0 else "Not in list"  ### an external return statement
+        return "Found " + str(found) + " in : \n" + " \n".join(
+            found_in) if found > 0 else "Not in list"  ### an external return statement
 
-
-    def delete_container(self, data):
+    def delete(self, data):
         container = self.main_holder
         previous = None
         while container:
-            if data == container.get_bid():
+            if data == container.get_data():
                 if container == self.main_holder:
                     self.main_holder = container.get_next_container()
 
@@ -79,30 +79,63 @@ class ContainerOfContainers:
                     previous.set_next_container(container.get_next_container())
                 return container
 
-            elif data != container.get_bid():
+            elif data != container.get_data():
                 previous = container
                 container = container.get_next_container()
         return "none"
 
+    def reverse(self):
 
-    def print_all_values(self):
+        container = self.main_holder
+
+        previous = None
+
+        while container:
+            next_container = container.get_next_container()
+            container.set_next_container(previous)
+            previous = container
+            container = next_container
+
+        self.main_holder = previous
+
+
+        #hold = []
+
+        #while container:
+        #    hold.insert(0, container.get_data())
+        #    container = container.get_next_container()
+
+        #container = self.main_holder
+        #count = 0
+
+        #while container:
+        #    container.set_data(hold[count])
+
+        #    container = container.get_next_container()
+        #    count += 1
+
+
+    def print(self):
         container = self.main_holder
         values = []
 
         while container:
-            values.append(container.get_bid())
+            values.append(container.get_data())
             container = container.get_next_container()
 
-        return ", ".join([str(item) for item in values]) if values != [] else "There are no values in the LinkedList"
+        print(", ".join([str(item) for item in values]) if values != [] else "There are no values in the LinkedList")
 
 
 li = ContainerOfContainers()
-li.contain_a_bid_and_a_box_if_any(1)
-li.contain_a_bid_and_a_box_if_any(4)
-li.contain_a_bid_and_a_box_if_any(2)
-li.contain_a_bid_and_a_box_if_any(3)
-li.contain_a_bid_and_a_box_if_any(4)
-li.contain_a_bid_and_a_box_if_any(5)
-print(li.search_containers_for_value(4), li.main_holder.next_container.next_container.next_container.get_bid()) #, li.main_holder.next_container.bid)
-#print(f'self.main_holder = {li.main_holder.next_container.next_container.get_bid()}')
-#li.delete_container(1)
+li.prepend("1")
+li.prepend("2")
+li.prepend("3")
+li.prepend("4")
+li.prepend("5")
+
+# print(f'self.main_holder = {li.main_holder.next_container.next_container.get_data()}')
+li.delete("3")
+print(li.main_holder.next_container.next_container.data)
+
+li.reverse()
+li.print()
